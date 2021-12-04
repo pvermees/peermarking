15df8e1f15ffa5b8d1c3d7fd82736d60b9733a33
pvermees <p.vermeesch@ucl.ac.uk>
2021-12-04 21:42:38 +0000

    Added Elo agorithm back in

--- mark.R
+++ mark.R
@@ -1,3 +1,5 @@
+require(PlayerRatings)
+
 dirs <- c('1-submissions','2-assignments','3-reviews','4-feedback','5-marks')
 
 readfeedback <- FALSE
@@ -5,22 +7,22 @@ readfeedback <- FALSE
 # 1. parse the review files
 ranks <- list()
 feedback <- list()
-for (marker in submissions){
-    ranks[[marker]] <- list(alias=NULL,student=NULL,rank=NULL)
-    feedbackfile <- paste0(dirs[3],'/',marker)
+for (m in submissions){
+    ranks[[m]] <- list(alias=NULL,student=NULL,rank=NULL)
+    feedbackfile <- paste0(dirs[3],'/',m)
     con <- file(feedbackfile, "r")
     for (line in readLines(con)){
         if (grepl('File name:',line)){
             string <- strsplit(line,'\\.')[[1]][1]
             a <- tail(strsplit(string,' ')[[1]],1)
             s <- submissions[which(aliases %in% a)]
-            ranks[[marker]]$alias <- c(ranks[[marker]]$alias,a)
-            ranks[[marker]]$student <- c(ranks[[marker]]$student,s)
+            ranks[[m]]$alias <- c(ranks[[m]]$alias,a)
+            ranks[[m]]$student <- c(ranks[[m]]$student,s)
         }
         if (grepl('Rank:',line)){
             string <- strsplit(line,'/')[[1]][1]
             r <- as.numeric(tail(strsplit(string,' ')[[1]],1))
-            ranks[[marker]]$rank <- c(ranks[[marker]]$rank,r)
+            ranks[[m]]$rank <- c(ranks[[m]]$rank,r)
         }
         if (readfeedback){
             if (grepl('----------------',line)){
@@ -46,11 +48,37 @@ for (s in names(feedback)){
 
 # 3. PGF file
 PGF <- ''
-for (s in names(ranks)){
-    i <- order(ranks[[s]]$rank)
+for (m in names(ranks)){
+    i <- order(ranks[[m]]$rank)
     part1 <- paste0(PGF,'task1 ',s,' ')
-    part2 <- paste(ranks[[s]]$student[i],collapse=' > ')
+    part2 <- paste(ranks[[m]]$student[i],collapse=' > ')
     part3 <- '\n'
     PGF <- paste0(part1,part2,part3)
     write(PGF,file=paste0(dirs[5],'/ranks.pgf'))
 }
+
+# 4. Elo table
+nr <- n*k*(k-1)/2
+tt <- rep(0,nr)
+p1 <- NULL
+p2 <- NULL
+score <- NULL
+for (m in submissions){
+    r <- ranks[[m]]
+    for (i in 1:(k-1)){
+        for (j in (i+1):k){
+            p1 <- c(p1,r$student[i])
+            p2 <- c(p2,r$student[j])
+            if (r$rank[i]>r$rank[j]){
+                score <- c(score,0)
+            } else if (r$rank[i]<r$rank[j]){
+                score <- c(score,1)
+            } else {
+                score <- c(score,1/2)
+            }
+        }
+    }
+}
+ELO <- data.frame(time=tt,p1=p1,p2=p2,score=score,stringsAsFactors=FALSE)
+rating <- elo(ELO)
+write.table(rating$ratings,file=paste0(dirs[5],'/elo.txt'))
